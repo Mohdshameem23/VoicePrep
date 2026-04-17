74cbffb08697c3ec2e2c83c99327eb2231f5dc7b
Mohd Shameem <istakeahmad34@gmail.com>
2026-04-17 16:33:17 +0530

    fix: build errors and downgrade next

--- ai-prepwise/next.config.mjs
+++ ai-prepwise/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
--- ai-prepwise/src/app/layout.js
+++ ai-prepwise/src/app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -22,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <Provider>
           {children}
