cb41323d5101470a23a603acd17299226c391489
Mohd Shameem <istakeahmad34@gmail.com>
2026-04-25 00:02:39 +0530

    added resume analyzer

--- ai-prepwise/src/app/(main)/dashboard/_components/CreateOption.jsx
+++ ai-prepwise/src/app/(main)/dashboard/_components/CreateOption.jsx
@@ -4,7 +4,14 @@ import { Phone } from 'lucide-react';
 import Link from 'next/link';
 export default function CreateOption() {
   return (
+
     <div className=' grid grid-cols-3 gap-5' >
+      <button
+        className="bg-blue-600 text-white rounded-lg px-4 py-2 font-semibold hover:bg-blue-700 transition-colors"
+        onClick={() => window.location.href = 'https://resumeiq-analyzer-zr1n.arcada.app'}
+      >
+        Go to ResumeIQ Analyzer
+      </button>
       <Link href={'/dashboard/create-interview'} className=' bg-white border-gray-200 rounded-lg p-5 flex flex-col gap-3 cursor-pointer'
 >
 <Video className='p-3 text-primary bg-blue-50 rounded-lg h-12 w-12'/>
